6d8a117fc27702691002f41ec7e2448ed2c53d1e
tsberg <tylersberg@gmail.com>
2024-05-31 15:49:38 -0700

    Data layout tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,15 @@ import { fetchCardData } from './data';
 //   )
 // }
 function PercentBar({label, value, total}: {label: string, value: number, total: number}) {
-  return (<ProgressBar className='py-2' value={(value / total) * 100} label={label} />)
+  return (
+    <div>
+      <p className='flex justify-between'>
+        <span>{label}</span>
+        <span className='justify-end'>{((value / total) * 100).toFixed(2)}% &bull; {value}/{total}</span>
+      </p>
+      <ProgressBar className='py-2' value={(value / total) * 100} />
+    </div>
+  )
 }
 export default async function Home() {
   const cd = await fetchCardData();
@@ -39,22 +47,28 @@ export default async function Home() {
         </input>
       </div>
       <div className='flex'>
-        <Card className='flex-1 justify-center m-4'>
-          Swing decisions
+        <Card className='flex-1 m-4'>
+          <h1 className='text-center'>Swing decisions</h1>
           <PercentBar label='Swing' value={cd.swing} total={cd.total}/>
-          <PercentBar label='Whiff'  value={cd.whiff} total={cd.total}/>
-          <PercentBar label='Foul'  value={cd.foul} total={cd.total}/>
-          <PercentBar label='In play'  value={cd.in_play} total={cd.total}/>
+          <div className='pl-4'>
+            <PercentBar label='Whiff'  value={cd.whiff} total={cd.total}/>
+            <PercentBar label='Foul'  value={cd.foul} total={cd.total}/>
+            <PercentBar label='In play'  value={cd.in_play} total={cd.total}/>
+          </div>
           <PercentBar label='Take'  value={cd.take} total={cd.total}/>
-          <PercentBar label='Ball'  value={cd.ball} total={cd.total}/>
-          <PercentBar label='Strike'  value={cd.strike} total={cd.total}/>
+          <div className='pl-4'>
+            <PercentBar label='Ball'  value={cd.ball} total={cd.total}/>
+            <PercentBar label='Strike'  value={cd.strike} total={cd.total}/>
+          </div>
         </Card>
-        <Card className='flex-1 justify-center m-4'>
-          Balls & Strikes
+        <Card className='flex-1 m-4'>
+          <h1 className='text-center'>Balls & Strikes</h1>
           <PercentBar label='Strike'  value={cd.strike} total={cd.total}/>
-          <PercentBar label='Whiff'  value={cd.whiff} total={cd.total}/>
-          <PercentBar label='Foul'  value={cd.foul} total={cd.total}/>
-          <PercentBar label='Called'  value={cd.called_strike} total={cd.total}/>
+          <div className='pl-4'>
+            <PercentBar label='Whiff'  value={cd.whiff} total={cd.total}/>
+            <PercentBar label='Foul'  value={cd.foul} total={cd.total}/>
+            <PercentBar label='Called'  value={cd.called_strike} total={cd.total}/>
+          </div>
           <PercentBar label='Ball'  value={cd.ball} total={cd.total}/>
         </Card>
       </div>
